cbc87b86880542a6a8374ce9d1e291abacb16917
QtChedy <raouf_najjar@hotmail.Com>
2016-01-24 08:29:50 +0000

    initial Club Video project commit

---
+++ src/Core/TestDB.java
@@ -0,0 +1,41 @@
+package Core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestDB {
+
+	public static void main(String[] args) {
+		/*try
+		{
+			StockManager mng = new StockManager();
+			//mng.addRealisateur(new Realisateur("chedy", "najjar"));
+			//mng.addFilm(new FilmAction("you", new Realisateur("chedy", "najjar"), 14.366));
+			
+	 
+	         ResultSet rset = mng.filterFilm("id_realisateur", (new Integer(new Realisateur("chedy", "najjar").hashCode())).toString());
+	 
+	         mng.removeRealisateur(new Realisateur("chedy", "najjar").hashCode());
+	         // Step 4: Process the ResultSet by scrolling the cursor forward via next().
+	         //  For each row, retrieve the contents of the cells with getXxx(columnName).
+	         System.out.println("The records selected are:");
+	         int rowCount = 0;
+	         while(rset.next()) {   // Move the cursor to the next row
+	            //int title = rset.getInt("id_realisateur");
+	            String price = rset.getString("titre");
+	            String cc   = rset.getString("prix");
+	            System.out.println(   price + ", " + cc);
+	            ++rowCount;
+	         }
+	         System.out.println("Total number of records = " + rowCount);
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}*/
+		
+			
+
+	}
+
+}
