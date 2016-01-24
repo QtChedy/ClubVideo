1044e1b20620f52dc1141a649583764fa5394f54
QtChedy <raouf_najjar@hotmail.Com>
2016-01-24 09:33:48 +0000

    added Jtable interface and button
    * building Data from ResultSet and passing it to JTabel

--- src/Gui/SupprimerFilm.java
+++ src/Gui/SupprimerFilm.java
@@ -2,11 +2,21 @@ package Gui;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JList;
 
+import Core.StockManager;
+import javax.swing.JTable;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import java.util.Vector;
+import java.sql.*;
+
 public class SupprimerFilm extends JPanel {
+	private StockManager mng;
+	private JTable table;
+	
+	
 
 	/**
 	 * Create the panel.
@@ -14,13 +24,48 @@ public class SupprimerFilm extends JPanel {
 	public SupprimerFilm() {
 		setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(32, 27, 389, 209);
-		add(list);
-		
 		JButton btnSupprimer = new JButton("Supprimer");
 		btnSupprimer.setBounds(178, 266, 89, 23);
 		add(btnSupprimer);
+		try
+		{
+		
+			mng = new StockManager();
+		
+			table = new JTable(buildTableModel(mng.getAllFilms()));
+		}
+		catch(SQLException e)
+		{
+			
+		}
+		table.setBounds(28, 32, 384, 210);
+		add(table);
+
+	}
+	
+	public static DefaultTableModel buildTableModel(ResultSet rs)
+	        throws SQLException {
+
+	    ResultSetMetaData metaData = rs.getMetaData();
+
+	    // names of columns
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add(metaData.getColumnName(column));
+	    }
+
+	    // data of the table
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (rs.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(rs.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    return new DefaultTableModel(data, columnNames);
 
 	}
 }
